c55d6b6ecc9576d35e5d113b51ec5841e856c5f7
Marcelo <dennermarcelopereira@gmail.com>
2025-03-09 14:12:51 -0300

    Resgate da Dados com API JSON

--- React/estudos react/src/App.jsx
+++ React/estudos react/src/App.jsx
@@ -1,4 +1,10 @@
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products"
+
 import './App.css';
+
+
 {/*arquivos em components*/}
 import FirstComponents from './components/FirstComponents';
 import TestMarcelo from './components/TestMarcelo';
@@ -14,8 +20,7 @@ const cars = [
   {id: 1, brand: 'veloster', km: 10000, color: 'vermelho'},
   {id: 2, brand: 'ferrari', km: 120000, color: 'preto'},
   {id: 3, brand: 'tesla', km: 2000, color: 'branco'},
-];
-
+]; 
 
 function App() {
 
@@ -24,6 +29,19 @@ function App() {
   };
 
   const redtitle = false;
+{/* renderizar usando api json no react*/}
+const [products, steProducts] = useState([]);
+
+useEffect(() => {
+  async function getData() {
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+  steProducts(data)
+  }
+  getData();
+}, []);
 
   return (
     
@@ -32,24 +50,37 @@ function App() {
         {/*<TestMarcelo /> está chamando o arquivo TestMarcelo.jsx que está dentro da pasta components,
         */}
 
+        
         <h1>Fundamentos do React</h1>
         {/*arquivos em components*/}
         <FirstComponents />
         <TestMarcelo />
+
         {/*imagem em assets*/}
         <img src={img} alt="" />
+
         {/*Destruturando props*/}
-        <CarDetaills brand='Fiat' km='10000' color='vermelho' />
-        {/*reaproveitando componentes*/}
+        <CarDetaills brand='Fiat' km='100' color='vermelho' />
+
+        {/*reaproveitando componentes
         <CarDetaills brand='BWM' km='120000' color='preto' />
-        <CarDetaills brand='PORSHE' km='2000' color='branco' />
+        <CarDetaills brand='PORSHE' km='2000' color='branco' />*/}
+
         {/*renderização de listas com componentes*/}
         {cars.map((car) => (
           <CarDetaills key={car.id} brand={car.brand} km={car.km} color={car.color} />
         ))}
 
         <he className={redtitle ? "red-title" : "title"}>vai ter uma classe</he>
+          <hr />
+        <h1 style={{color: "red"}}>HTTP com REACT</h1>
         
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.name} - R${product.price}</li>
+          ))}
+        </ul>
+
       </div>
       
   )
